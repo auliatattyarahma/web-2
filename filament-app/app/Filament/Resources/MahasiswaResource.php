ba1fc76033cc529f8d2c303d3bc38f8ad029eeb6
Aulia Tattya Rahma <emailkamu@example.com>
2025-06-07 11:19:22 +0700

    Menambahkan beberapa file baru

--- filament-app/app/Filament/Resources/MahasiswaResource.php
+++ filament-app/app/Filament/Resources/MahasiswaResource.php
@@ -18,7 +18,11 @@ class MahasiswaResource extends Resource
 {
     protected static ?string $model = Mahasiswa::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-academic-cap';
+
+    protected static ?string $navigationLabel = "Mahasiswa";
+    protected static ?string $pluralModelLabel = "Mahasiswa";
+    protected static ?string $slug = "Mahasiswa";
 
     public static function form(Form $form): Form
     {
@@ -27,16 +31,25 @@ class MahasiswaResource extends Resource
                 Forms\Components\TextInput::make('nama')
                     ->required(),
                 Forms\Components\TextInput::make('nim')
+                    ->unique()
                     ->required(),
                 Forms\Components\TextInput::make('email')
                     ->email()
+                    ->unique()
                     ->required(),
                 Forms\Components\TextInput::make('telepon')
                     ->tel(),
                 Forms\Components\TextInput::make('alamat'),
                 Forms\Components\DatePicker::make('tanggal_lahir'),
                 Forms\Components\TextInput::make('jurusan'),
-                Forms\Components\TextInput::make('foto'),
+                Forms\Components\FileUpload::make('foto')
+                    ->image()
+                    ->directory('foto_mahasiswa')
+                    ->maxSize(5120)
+                    ->nullable()
+                    ->openable()
+                    ->downloadable()
+                    ->nullable(),
                 Forms\Components\TextInput::make('status')
                     ->required(),
                 Forms\Components\TextInput::make('angkatan'),
@@ -64,7 +77,7 @@ class MahasiswaResource extends Resource
                     ->sortable(),
                 Tables\Columns\TextColumn::make('jurusan')
                     ->searchable(),
-                Tables\Columns\TextColumn::make('foto')
+                Tables\Columns\ImageColumn ::make('foto')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('status')
                     ->searchable(),
@@ -85,14 +98,26 @@ class MahasiswaResource extends Resource
             ])
             ->filters([
                 SelectFilter::make('jurusan')
-                    ->options(Mahasiswa::distinct()->pluck('jurusan', 'jurusan')->toArray()),
+                    ->options(
+                        Mahasiswa::query() 
+                            ->whereNotNull('jurusan') 
+                            ->distinct()
+                            ->pluck('jurusan', 'jurusan')
+                            ->toArray()
+                    ),
                 SelectFilter::make('jenis_kelamin')
                     ->options([
                         'L' => 'Laki-laki',
                         'P' => 'Perempuan',
                     ]),
                 SelectFilter::make('agama')
-                    ->options(Mahasiswa::distinct()->pluck('agama', 'agama')->toArray()),
+                    ->options(
+                        Mahasiswa::query() // Gunakan query() untuk memulai query builder
+                            ->whereNotNull('agama') // Tambahkan ini: hanya ambil yang agamanya tidak NULL
+                            ->distinct()
+                            ->pluck('agama', 'agama')
+                            ->toArray()
+                    ),
                 SelectFilter::make('status')
                     ->options([
                         'aktif' => 'Aktif',
